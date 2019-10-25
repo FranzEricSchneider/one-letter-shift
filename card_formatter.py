5306e133322a7eee3ced5660df375dbf59d6d75b
Eric <eric@righthandrobotics.com>
2019-10-25 18:32:43 -0400

    Added some notes

--- card_formatter.py
+++ card_formatter.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 
+"""
+TODO:
+    Polish the tests and docstrings
+
+    Sort things appropriately by level and interest
+    Color code the boxes
+    Make a mechanism for bolding things
+    Get the color highlighting right for the right fields (LaTeX first)
+    Format school and labels differently
+"""
+
 import argparse
 import csv
 
-
-
-# Problematic strings to find and replace 
+# Problematic strings to find and replace
 BAD_STRINGS = {}
 
 
 def filter_values(rows, bad_strings):
     for row in rows:
-        print("row", row)
         for key in row:
-            print("key", key)
             for bad in bad_strings:
-                print("bad", bad)
-                print("row[key]", row[key])
                 if bad in row[key]:
                     print("{!r} found in {}, replaced with {!r}".format(
                         bad, row[key], bad_strings[bad]
@@ -95,6 +100,9 @@ def card_formatter(arrangement, width, to_file=False):
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("csv", help="CSV to draw card data from")
+    parser.add_argument("-f", "--to-file",
+                        help="Writes tex code to text.tex instead of printing",
+                        action="store_true")
     args = parser.parse_args()
 
     # First get the fields
@@ -107,12 +115,9 @@ def main():
         csv_reader = csv.DictReader(csv_file, delimiter=",")
         rows = [row for row in csv_reader]
 
-    # for k, v in arranger(fields, rows, width=3, height=6).items():
-    #     print (k, v)
-
     card_formatter(arranger(fields, rows, width=3, height=6),
                    width=3,
-                   to_file=True)
+                   to_file=args.to_file)
 
 
 if __name__ == "__main__":
